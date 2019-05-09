28207484276daab6b18f5989c1bd1367b9eb5498
vapasi <vapasi@vapasis-MacBook-Pro.local>
2019-05-09 07:30:57 +0530

    added system property for key and token to get from jenkins

--- src/test/java/BaseTest.java
+++ src/test/java/BaseTest.java
@@ -14,8 +14,11 @@ import static io.restassured.RestAssured.given;
 
 public class BaseTest {
 
-    public final String token = "bf0af85fd0e2bfbdd384420cd377192067e780cca8b1b756090bebff88dcc633";
-    public final String key = "63653eea2f3c36c805323c8fe3e6cf79";
+   // public final String token = "bf0af85fd0e2bfbdd384420cd377192067e780cca8b1b756090bebff88dcc633";
+   // public final String key = "63653eea2f3c36c805323c8fe3e6cf79";
+
+    public final String token = System.getProperty("token");
+    public final String key = System.getProperty("key");
 
     protected String boardId;
     public String boardName = "Board RestAssured";
